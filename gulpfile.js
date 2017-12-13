0702ee5cf290ddf97eddeef6f8858d642a193026
darthvoider <voiderok@gmail>
2017-12-13 22:56:00 +0200

    ADD: gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var fs = require( 'fs' );
+var gulp = require( 'gulp' );
+var plugins = require( 'gulp-load-plugins' )( {
+	rename: {
+		'gulp-line-ending-corrector': 'lec',
+		'gulp-clean-css': 'minify'
+	}
+} );
+
+global.config = JSON.parse( fs.readFileSync( './gulp/config/config.json' ) );
+
+function getTask( task ) {
+	return require( './gulp/tasks/' + task )( gulp, plugins );
+}
+
+gulp.task( 'sass', getTask( 'sass' ) );
+gulp.task( 'replace', getTask( 'newtheme' ) );
+
+gulp.task( 'default', [ 'sass' ], function () {
+	gulp.watch( config.root.themePath + config.root.themeName + config.sass.watch, [ 'sass' ] );
+} );
+
+
+
+
