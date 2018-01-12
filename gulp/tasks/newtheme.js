a019c00de5da8a9073f2ee72f92268ffbaa7751e
darthvoider <voiderok@gmail>
2018-01-12 14:03:09 +0200

    UPD: replace task

--- gulp/tasks/newtheme.js
+++ gulp/tasks/newtheme.js
@@ -1,6 +1,6 @@
 module.exports = function ( gulp, plugins ) {
 	return function () {
-		gulp.src( [ config.root.themePath + config.root.themeName + "**/*[.php, .js, .scss, .css, .pot, .xml]" ] )
+		gulp.src( [ config.root.themePath + config.root.themeName + "**/*[.php, .js, .scss, .css, .pot, .xml, .json]" ] )
 			.pipe( plugins.replace( config.replace.oldtheme, config.replace.newtheme ) )
 			.pipe( plugins.replace( config.replace.OldTheme, config.replace.NewTheme ) )
 			.pipe( plugins.replace( config.replace.OLDTHEME, config.replace.NEWTHEME ) )
